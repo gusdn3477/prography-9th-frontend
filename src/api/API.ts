26190fad3dbc2ff1f33b81ea5105283c434932d5
gusdn3477 <gusdn3477@naver.com>
2024-02-03 13:15:00 +0900

    feat: api 함수 스펙 변경

--- src/api/API.ts
+++ src/api/API.ts
@@ -2,16 +2,27 @@ import axios from 'axios';
 
 const BaseURL = 'https://www.themealdb.com';
 
-export const get = async () => {
-  const response = await axios.get(`${BaseURL}/api/json/v1/1/categories.php`);
-  const { status, data } = response;
-  return { status, data };
+export const getCategories = async () => {
+  try {
+    const response = await axios.get(`${BaseURL}/api/json/v1/1/categories.php`);
+    return response.data.categories;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-export const getWithCategory = async (strCategory: string) => {
-  const response = await axios.get(`
-  ${BaseURL}/api/json/v1/1/filter.php?c=${strCategory}`);
-
-  const { status, data } = response;
-  return { status, data };
+export const getItems = async (strCategory?: string) => {
+  try {
+    let response = undefined;
+    if (strCategory)
+      response = await axios.get(
+        `${BaseURL}/api/json/v1/1/filter.php?c=${strCategory}`
+      );
+    else {
+      response = await axios.get(`${BaseURL}/api/json/v1/1/filter.php`);
+    }
+    return response.data.meals;
+  } catch (e) {
+    console.log(e);
+  }
 };
