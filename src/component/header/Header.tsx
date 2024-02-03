4d59243a9db27446d0a7285caf84509422e335dc
gusdn3477 <gusdn3477@naver.com>
2024-02-03 10:50:00 +0900

    feat: 헤더 컴포넌트 구현

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,3 +1,16 @@
+import styled from '@emotion/styled';
+import prographyLogo from '/prography.png';
+
 export const Header = () => {
-  return <div></div>;
+  return (
+    <StyledHeader>
+      <a href="https://prography.org/" target="_blank">
+        <img src={prographyLogo} className="logo react" alt="Prography Logo" />
+      </a>
+    </StyledHeader>
+  );
 };
+
+const StyledHeader = styled.div`
+  padding: 10px;
+`;
