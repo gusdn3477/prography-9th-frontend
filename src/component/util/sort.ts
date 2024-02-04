5965cdbfcbe0d8f871c51e91b600e56a1b9a8ccc
gusdn3477 <gusdn3477@naver.com>
2024-02-04 11:58:04 +0900

    feat: filter에 따라 정렬하는 함수 구현

---
+++ src/component/util/sort.ts
@@ -0,0 +1,17 @@
+import { MealModel } from '../../model';
+
+export const sort = (
+  data: MealModel[],
+  filter: 'now' | 'ascending' | 'decending'
+) => {
+  const _data = [...data];
+  if (filter === 'now') {
+    _data.sort((a, b) => Number(a.idMeal) - Number(b.idMeal));
+  } else if (filter === 'ascending') {
+    _data.sort((a, b) => a.strMeal.localeCompare(b.strMeal));
+  } else {
+    _data.sort((a, b) => b.strMeal.localeCompare(a.strMeal));
+  }
+
+  return _data;
+};
